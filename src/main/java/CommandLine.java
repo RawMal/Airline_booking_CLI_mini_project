d732457febacdab0c4e7776e18d95d66d02dbbe0
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-09-17 17:16:03 +0100

    created addFlight method

--- src/main/java/CommandLine.java
+++ src/main/java/CommandLine.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CommandLine {
 
@@ -14,5 +15,16 @@ public class CommandLine {
         this.isOperational = true;
     }
 
-    public void addFlight
+    public void addFlight(){
+        System.out.println("Enter the Destination you would like to add to the system:");
+        Scanner inputScanner = new Scanner(System.in); // creating the scanner
+        String destination = inputScanner.nextLine(); // creating line where the user inputs destination
+        Flight newFlight = new Flight(destination); // where the scanner will add the information
+        addFlight(newFlight);
+        System.out.println("Flight added!");
+    }
+
+    public void addFlight(Flight flight) {
+        listOfFlights.add(flight);
+    }
 }
