517a548f5328364208c2eeb82fd561ab3ebcf836
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-09-17 16:10:22 +0100

    added methods and getsets to Passenger

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,20 @@
 public class Passenger {
+
+    String name;
+    String contactNumber;
+    int passengerID;
+
+    public Passenger(String name, String contactNumber, int passengerID){
+        this.name = name;
+        this.contactNumber = contactNumber;
+        this.passengerID = passengerID;
+    }
+
+    public int getPassengerID() {
+        return passengerID;
+    }
+
+    public void setPassengerID(int passengerID) {
+        this.passengerID = passengerID;
+    }
 }
