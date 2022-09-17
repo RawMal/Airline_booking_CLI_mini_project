93d0a66a5ccbfc6f16a65113d1115d5aadc68d06
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-09-17 16:55:52 +0100

    CL constructor

---
+++ src/main/java/CommandLine.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class CommandLine {
+
+    private ArrayList<Flight> listOfFlights;
+
+    private ArrayList<Passenger> passengerRepo;
+
+    private boolean isOperational;
+
+    public CommandLine() {
+        this.listOfFlights = new ArrayList<>();
+        this.passengerRepo = new ArrayList<>();
+        this.isOperational = true;
+    }
+
+    public void addFlight
+}
