8156fcb803cd251331f5120983c76208f71fd2d3
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-09-17 16:23:21 +0100

    added methods and getsets to flight

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,6 +1,51 @@
 import java.util.ArrayList;
 
 public class Flight {
+    private String destination;
+    private int flightId;
+    private ArrayList<Passenger> passengersOnTheFlight;
+    private boolean isCancelled;
 
+    public Flight (String destination, int flightId){
+        this.destination = destination;
+        this.flightId = flightId;
+        this.passengersOnTheFlight = new ArrayList<>();
+        this.isCancelled = false;
+    }
 
+    public void addPassengerToFlight(Passenger passenger){ // Adds a passenger to the arraylist in passenger class
+        passengersOnTheFlight.add(passenger);
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(int flightId) {
+        this.flightId = flightId;
+    }
+
+    public ArrayList<Passenger> getPassengersOnTheFlight() {
+        return passengersOnTheFlight;
+    }
+
+    public void setPassengersOnTheFlight(ArrayList<Passenger> passengersOnTheFlight) {
+        this.passengersOnTheFlight = passengersOnTheFlight;
+    }
+
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        isCancelled = cancelled;
+    }
 }
